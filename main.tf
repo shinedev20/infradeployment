98cd8b0a2daf3dcd435a81f6a5b411bf6ec953d9
shinedev20 <findhighincomejobinpune@gmail.com>
2022-05-16 22:35:27 +0530

    added service resource

--- main.tf
+++ main.tf
@@ -42,11 +42,10 @@ resource "azurerm_subnet" "subnet" {
   resource_group_name  = azurerm_resource_group.rg0123.name
   virtual_network_name = azurerm_virtual_network.vnet.name
   address_prefixes     = ["10.0.2.0/24"]
-  service_resources = [
-      azurerm_resource_group.rg0123.id,
-      azurerm_storage_account.stg1.id
-    ]
+  service_endpoints    = ["Microsoft.Storage"]
 }
+
+
 output "subnet" {
   value = "azurerm_subnet.subnet.id"
 }
@@ -56,10 +55,7 @@ resource "azurerm_subnet" "subnet1" {
   resource_group_name  = azurerm_resource_group.rg0123.name
   virtual_network_name = azurerm_virtual_network.vnet.name
   address_prefixes     = ["10.0.3.0/24"]
-  service_resources = [
-      azurerm_resource_group.rg0123.id,
-      azurerm_storage_account.stg1.id
-    ]
+  service_endpoints    = ["Microsoft.Storage"]
 }
 output "subnet1" {
   value = "azurerm_subnet.subnet1.id"
