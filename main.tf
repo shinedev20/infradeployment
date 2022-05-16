e0106533d0d6b3fefa6575765b954c997302987f
shinedev20 <findhighincomejobinpune@gmail.com>
2022-05-16 22:00:49 +0530

    added network and mi

--- main.tf
+++ main.tf
@@ -43,6 +43,12 @@ resource "azurerm_subnet" "subnet" {
   virtual_network_name = azurerm_virtual_network.vnet.name
   address_prefixes     = ["10.0.2.0/24"]
 }
+resource "azurerm_subnet" "subnet1" {
+  name                 = join("-", [var.env, var.reg, var.dom,"subnet",var.index1])
+  resource_group_name  = azurerm_resource_group.rg0123.name
+  virtual_network_name = azurerm_virtual_network.vnet.name
+  address_prefixes     = ["10.0.3.0/24"]
+}
 /*
 
 resource "azurerm_network_interface" "nic" {
@@ -95,5 +101,19 @@ resource "azurerm_storage_account" "stg1" {
   location                 = var.location
   name                     = join("", [var.env, var.reg, var.dom, "stg", var.index1])
   resource_group_name      = azurerm_resource_group.rg013.name
+}
+resource "azurerm_storage_account_network_rules" "test" {
+  storage_account_id = azurerm_storage_account.stg1.id
+
+  default_action             = "Allow"
+  ip_rules                   = ["127.0.0.1"]
+  virtual_network_subnet_ids = [azurerm_subnet.subnet.id,azurerm_subnet.subnet1.id]
+  bypass                     = ["Metrics"]
+}
+resource "azurerm_user_assigned_identity" "example" {
+  resource_group_name = azurerm_resource_group.rg0123.name
+  location            = var.location
+
+  name = join("-", [var.env, var.reg,"mi"])
 }
 
