8e0eebf98afd9cfbbf7c0d21c9f69b48befc8ff9
shinedev20 <findhighincomejobinpune@gmail.com>
2022-05-17 21:40:53 +0530

    added keyvault

--- main.tf
+++ main.tf
@@ -209,7 +209,7 @@ resource "azurerm_key_vault" "example" {
   }
   access_policy {
     tenant_id = data.azurerm_client_config.current.tenant_id
-    object_id = azurerm_user_assigned_identity.mi.client_id
+    object_id = azurerm_user_assigned_identity.mi.principal_id
 
     key_permissions = [
       "Get",
